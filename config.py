8a6b1ddb434f1fd95f1524b3d4f0d5b8ea69ce36
Thomas Minh Nguyen <thomasnguyen8503@gmail.com>
2026-06-16 14:35:10 +1200

    config changes

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import pygame
+from item import Item
 
 if not pygame.get_init():
     pygame.display.init()
@@ -156,3 +157,62 @@ BUTTON_INVENTORY_WIDTH = 120
 BUTTON_INVENTORY_HEIGHT = 40
 BUTTON_INVENTORY_X = WINDOW_WIDTH - 130
 BUTTON_INVENTORY_Y = WINDOW_HEIGHT - 50
+
+#Items
+#---Weapons---
+ITEM_NAVIGATION_STICK = Item("Navigation Stick", "stamina", 100, "weapon",
+    "Amazingly, these are not as common as you'd believe. Before they reach adulthood, the villagers slowly map out every section of the village through humiliating trial and error, trial and error which involves falling into the water more often than not. You assume this is for a child that hasn't gained that pattern recognition yet. Poor child.")
+ITEM_MURKLURKER_CLUB = Item("Murklurker Club", "stamina", 15, "weapon",
+    "A club with Murklurker teeth. Ironically, it's not very effective against the Murklurkers themselves. The Pariahs tried once. They never tried again.")
+ITEM_PARIAH_SPEAR = Item("Pariah Spear", "stamina", 20, "weapon",
+    "A crude spear crafted by the sighted pariahs for hunting. Its long reach makes it effective against Murklurkers, Pirates, and Island Tribesmen alike. To be killed by it is an ultimate insult.")
+ITEM_SACRIFICIAL_KNIFE = Item("Sacrificial Knife", "stamina", 12, "weapon",
+    "Wielded by the apostles to offer an unfortunate victim to the Lighthouse each week. Dying by it supposedly grants an eternity of bliss underneath the Lighthouse's gaze. It's probably best not to test the theory.")
+ITEM_LANTERN_FLAIL = Item("Lantern Flail", "stamina", 25, "weapon",
+    "Several small lanterns are tethered to a stick by especially strong seaweed. Contemplating how strange and impractical the weapon seems is a surefire way to be killed by it. It seems to amuse the Murklurkers.")
+
+#---Body---
+ITEM_SEAWEED_VEST = Item("Seaweed Vest", "health", 20, "body",
+    "When the villagers have nothing better to do, they wander into the ocean and search for seaweed. Some of them have weaved clothing from it. It's cold and slimy, but surprisingly sturdy.")
+ITEM_STILT_BOX = Item("Stilt Box", "health", 35, "body",
+    "It's a blessing in disguise the villagers can't see, because this looks hideous. In theory, it's meant to help Pariahs when a Murklurker makes their way onto the stilts. Even the Murklurkers laugh at the outfit.")
+ITEM_CEREMONIAL_ROBE = Item("Ceremonial Robe", "health", 36, "body",
+    "An emerald-green robe damp with seawater and blood that the apostles wear during sacrifices. It offers little protection, but you'll look fabulous if you wear it.")
+ITEM_CLOAK_OF_DEVOTEE = Item("Cloak of the Devotee", "health", 30, "body",
+    "Contrary to the name, it's actually a poncho. A gaudily colored cloth poncho that somehow makes the cultists look even creepier. Only the most devoted are granted the privilege to wear it.")
+
+#---Head---
+ITEM_MURKLURKER_SKULL = Item("Murklurker Skull", "damage_reduction", 8, "head",
+    "The pariahs sometimes wear these for bragging rights. The villagers never notice. They're blind.")
+ITEM_MURKLURKER_CAGE = Item("Murklurker Cage", "damage_reduction", 15, "head",
+    "A cage worn by pariahs to humiliate them. It is incredibly difficult to take off, but offers some protection against the Murklurkers.")
+ITEM_LANTERN_HELMET = Item("Lantern Helmet", "damage_reduction", 13, "head",
+    "Some of the villagers wear these to showcase their dedication to the Lighthouse. Some refuse to, fearing it would insult their God. Theological debates around this are common, and occasionally violent.")
+ITEM_SEAWEED_HELMET = Item("Seaweed Helmet", "damage_reduction", 10, "head",
+    "The blinded children are taught to make these to help them cope with their recent transformation. The quality is judged by the length of the seaweed and the amount of knots. They take it seriously.")
+
+#---Legs---
+ITEM_WOODWEED_LEGGINGS = Item("Woodweed Leggings", "stamina_regen", 10, "legs",
+    "The villagers wear this for comfort and the pariahs wear it for protection. It doesn't really offer either, but in this village, nothing does.")
+ITEM_TORN_PIRATE_LEGGINGS = Item("Torn Pirate Leggings", "stamina_regen", 8, "legs",
+    "Stolen from the pirates in Trash Vortex during a raid. Those pirates didn't go down without a fight. They also didn't go down without ruining their own pants. Rude.")
+ITEM_KNEELING_PADS = Item("Kneeling Pads", "stamina_regen", 12, "legs",
+    "To be unable to pray to the Lighthouse is one of the most unforgivable sins in the village! Even the most psychotic of cultists understand the importance of proper leg-joint safety.")
+
+#---Food / Misc---
+ITEM_LIGHTBALL = Item("Lightball", "food", 21, description=
+    "A ball of light, granted physical form by the Lighthouse for an unknown but likely nefarious reason. The good children get to play with these.")
+ITEM_BROKEN_PLANK = Item("Broken Plank", "food", 14, description=
+    "A plank that probably was meant to be part of a hut. One of the Pariahs dropped it, robbing it of its purpose. It's safe to say they were sacrificed for this mistake.")
+ITEM_WOODEN_LANTERN = Item("Wooden Lantern", "food", 23, description=
+    "Pariahs that are sacrificed are freed of their stigma, and granted the right to spend an eternity underneath the light. These lanterns are hung up to show their bravery.")
+ITEM_MURKLURKER_TOOTH = Item("Murklurker Tooth", "food", 12, description=
+    "These teeth slice through skin faster than most knives. Carrying these around without proper care is a good way to die of blood loss.")
+ITEM_MURKLURKER_POOP = Item("Murklurker Poop", "food", 1, description=
+    "Why the fuck would you pick this up? This is as horrific as you can imagine.")
+ITEM_MURKLURKER_FLESH = Item("Murklurker Flesh", "food", 13, description=
+    "The food of the Pariahs. It is heavy, looks awful, smells awful, tastes awful. It provides both food and water, but is only marginally better than dying from a lack of either.")
+ITEM_SEAWEED = Item("Seaweed", "food", 2, description=
+    "Gross, smelly seaweed. You could wear it on your nose to torment real estate agents if they still existed.")
+ITEM_LIGHTHOUSE_HEART = Item("Lighthouse Heart", "food", 100, description=
+    "The heart of an evil God. How the Lighthouse gained sentience or why it became so sadistic will be lost to time. This can be sold for a large amount of food... Or thrown into a fire.")
--- enemy.py
+++ enemy.py
@@ -1,5 +1,5 @@
 import random
-from item import Item
+from config import *
 
 _NORMALIZE = str.maketrans({
     '‘': "'", '’': "'",
@@ -33,47 +33,51 @@ class EnemyVillager(EnemyBase):
     def __init__(self):
         super().__init__("Villager", 30, [
             "I hear you, sacrifice. Stand still and embrace your fate",
-            "The Light, the Light! Isn’t it wonderful?",
+            "The Light, the Light! Isn't it wonderful?",
             "You are here for a reason. Surrender to the Light!",
             "Your eyes led you to your destruction. darkness awaits",
             "The Lighthouse guides us! He provides us with sustenance!",
             "Run away, and one of us dies instead!",
         ], [
             None,
-            Item("Murklurker Club",   "stamina",       15, "weapon", "A club with Murklurker teeth. Ironically, it’s not very effective against the Murklurkers themselves. The Pariahs tried once. They never tried again."),
-            Item("Seaweed Vest",      "health",        20, "body",   "When the villagers have nothing better to do, they wander into the ocean and search for seaweed. Some of them have weaved clothing from it. It’s cold and slimy, but surprisingly sturdy."),
-            Item("Woodweed Leggings", "stamina_regen", 10, "legs",   "The villagers wear this for comfort and the pariahs wear it for protection. It doesn’t really offer either, but in this village, nothing does."),
-        ], [22, 19, 22, 37])
+            ITEM_NAVIGATION_STICK,
+            ITEM_MURKLURKER_CLUB,
+            ITEM_SEAWEED_VEST,
+            ITEM_WOODWEED_LEGGINGS,
+            ITEM_LIGHTBALL,
+        ], [10, 40, 10, 15, 15, 10])
 
 class EnemyPariah(EnemyBase):
     def __init__(self):
         super().__init__("Pariah", 30, [
-            "So the murklurkers didn’t get you? I will",
-            "At least you’re not a murklurker",
+            "So the murklurkers didn't get you? I will",
+            "At least you're not a murklurker",
             "We all have a purpose here. Even you... Even me",
-            "I wish they could see the Light as I can. It’s beautiful…",
+            "I wish they could see the Light as I can. It's beautiful...",
             "The murklurkers fear me! You will soon know why."
         ], [
-            Item("Pariah Spear",         "stamina",          20, "weapon", "A crude spear crafted by the sighted pariahs for hunting. Its long reach makes it effective against Murklurkers, Pirates, and Island Tribesmen alike. To be killed by it is an ultimate insult."),
-            Item("Stilt Box",            "health",           35, "body",   "It’s a blessing in disguise the villagers can’t see, because this looks hideous. In theory, it’s meant to help Pariahs when a Murklurker makes their way onto the stilts. Even the Murklurkers laugh at the outfit."),
-            Item("Murklurker Skull",     "damage_reduction",  8, "head",   "The pariahs sometimes wear these for bragging rights. The villagers never notice. They’re blind."),
-            Item("Murklurker Cage",      "damage_reduction", 15, "head",   "A cage worn by pariahs to humiliate them. It is incredibly difficult to take off, but offers some protection against the Murklurkers."),
-            Item("Woodweed Leggings",    "stamina_regen",    10, "legs",   "The villagers wear this for comfort and the pariahs wear it for protection. It doesn’t really offer either, but in this village, nothing does."),
-            Item("Torn Pirate Leggings", "stamina_regen",     8, "legs",   "Stolen from the pirates in Trash Vortex during a raid. Those pirates didn’t go down without a fight. They also didn’t go down without ruining their own pants. Rude."),
-        ], [8, 10, 17, 7, 26, 35])
+            ITEM_PARIAH_SPEAR,
+            ITEM_STILT_BOX,
+            ITEM_MURKLURKER_SKULL,
+            ITEM_MURKLURKER_CAGE,
+            ITEM_WOODWEED_LEGGINGS,
+            ITEM_TORN_PIRATE_LEGGINGS,
+            ITEM_BROKEN_PLANK,
+        ], [8, 10, 17, 7, 26, 35, 15])
 
 class EnemyApostle(EnemyBase):
     def __init__(self):
         super().__init__("Apostle", 30, [
-            "O’ Mighty Lighthouse, grant me strength!",
+            "O' Mighty Lighthouse, grant me strength!",
             "To reject the sins of the eyes! An act of salvation!",
             "Your death shall be quick! We offer you mercy!",
             "One moment of pain, and you rest in the Light for eternity!",
             "The Lighthouse is a kind God. You demonstrate His mercy."
         ], [
             None,
-            Item("Sacrificial Knife", "stamina", 12, "weapon", "Wielded by the apostles to offer an unfortunate victim to the Lighthouse each week. Dying by it supposedly grants an eternity of bliss underneath the Lighthouse’s gaze. It’s probably best not to test the theory."),
-        ], [64, 36])
+            ITEM_SACRIFICIAL_KNIFE,
+            ITEM_CEREMONIAL_ROBE,
+        ], [14, 36, 50])
 
 class EnemyMurklurker(EnemyBase):
     def __init__(self):
@@ -85,9 +89,13 @@ class EnemyMurklurker(EnemyBase):
             "Gurgar Glargh"
         ], [
             None,
-            Item("Murklurker Tooth", "food",  12, description="These teeth slice through skin faster than most knives. Carrying these around without proper care is a good way to die of blood loss."),
-            Item("Murklurker Poop",  "food",   1, description="Why the fuck would you pick this up? This is as horrific as you can imagine."),
-        ], [66, 33, 1])
+            ITEM_MURKLURKER_TOOTH,
+            ITEM_MURKLURKER_POOP,
+            ITEM_MURKLURKER_FLESH,
+            ITEM_SEAWEED,
+            ITEM_MURKLURKER_CLUB,
+            ITEM_MURKLURKER_SKULL,
+        ], [20, 25, 2, 10, 8, 25, 10])
 
 class EnemyDominantMurklurker(EnemyBase):
     def __init__(self):
@@ -98,9 +106,13 @@ class EnemyDominantMurklurker(EnemyBase):
             "Glarrrrghhhh Gugharrrrgh Guhargh"
         ], [
             None,
-            Item("Murklurker Tooth", "food",  12, description="These teeth slice through skin faster than most knives. Carrying these around without proper care is a good way to die of blood loss."),
-            Item("Murklurker Poop",  "food",   1, description="Why the fuck would you pick this up? This is as horrific as you can imagine."),
-        ], [66, 33, 1])
+            ITEM_MURKLURKER_TOOTH,
+            ITEM_MURKLURKER_POOP,
+            ITEM_MURKLURKER_FLESH,
+            ITEM_SEAWEED,
+            ITEM_MURKLURKER_CLUB,
+            ITEM_MURKLURKER_SKULL,
+        ], [20, 25, 2, 10, 8, 25, 10])
 
     def set_new_prompt(self):
         self.current_prompt = self.prompt[self.count]
@@ -111,17 +123,19 @@ class EnemyDominantMurklurker(EnemyBase):
 class EnemyFanatic(EnemyBase):
     def __init__(self):
         super().__init__("Fanatic", 30, [
-            "THE LIGGIGHIHT! IT’S BEAUTiFUL!",
+            "THE LIGGIGHIHT! IT'S BEAUTiFUL!",
             "FROMDARKNESS CoMeS LiiGht!",
             "THeLi.ght WiLL GuiuiDE YOu!",
             "ReMoVe YOURUR EyE!S GaIN SaLVATION!",
             "CoMEE!E! To T!H!E Liighht! Be FoRgiVEn!"
         ], [
             None,
-            Item("Lantern Flail",        "stamina",       25, "weapon", "Several small lanterns are tethered to a stick by especially strong seaweed. Contemplating how strange and impractical the weapon seems is a surefire way to be killed by it. It seems to amuse the Murklurkers."),
-            Item("Cloak of the Devotee", "health",        30, "body",   "Contrary to the name, it’s actually a poncho. A gaudily colored cloth poncho that somehow makes the cultists look even creepier. Only the most devoted are granted the privilege to wear it."),
-            Item("Kneeling Pads",        "stamina_regen", 12, "legs",   "To be unable to pray to the Lighthouse is one of the most unforgivable sins in the village! Even the most psychotic of cultists understand the importance of proper leg-joint safety."),
-        ], [14, 28, 28, 30])
+            ITEM_LANTERN_FLAIL,
+            ITEM_CLOAK_OF_DEVOTEE,
+            ITEM_KNEELING_PADS,
+            ITEM_LANTERN_HELMET,
+            ITEM_WOODEN_LANTERN,
+        ], [10, 20, 20, 17, 23, 10])
 
 class EnemyTeacher(EnemyBase):
     def __init__(self):
@@ -133,8 +147,10 @@ class EnemyTeacher(EnemyBase):
             "We are not insane! The Lighthouse grants us freedom from ourselves!",
             "You have to understand! Without us, who will take care of the children?",
             "They are so young, so lost. We are here for them! For all of them!"
-        ], [None], [100])
-
+        ], [
+            None,
+            ITEM_SEAWEED_HELMET,
+        ], [60, 40])
 
     def set_new_prompt(self):
         self.current_prompt = self.prompt[self.count]
@@ -151,7 +167,9 @@ class EnemyLighthouse(EnemyBase):
             "I have all the time in the world. Unlike you, death does not hinder me. Sit down and listen.",
             "The village you escaped. I am their nourishment. Their protection. Their guide. Their father. Their God.",
             "They came to me lost, scared, and confused. I freed them of their limitations, saved them from the Murklurkers."
-        ], [Item("Lighthouse Heart", "food", 100, description="The heart of an evil God. How the Lighthouse gained sentience or why it became so sadistic will be lost to time. This can be sold for a large amount of food... Or thrown into a fire.")], [100])
+        ], [
+            ITEM_LIGHTHOUSE_HEART,
+        ], [100])
 
     def set_new_prompt(self):
         self.current_prompt = self.prompt[self.count]
